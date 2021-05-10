1d8ce2fc78ea9d7268d8158ec021d300ee546317
bopipilala <1611326587@qq.com>
2021-05-11 01:03:47 +0800

    新增自定义组件：输入框+Title

---
+++ components/inputComponent/inputComponent.js
@@ -0,0 +1,36 @@
+// components/inputComponent/inputComponent.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    name: {
+      type: String,
+    },
+    isPassword: {
+      type: Boolean,
+      value: false
+    },
+    placeholder: {
+      type: String,
+      value: 'input框的置位字'
+    },
+    inputName: {
+      type: String,
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
